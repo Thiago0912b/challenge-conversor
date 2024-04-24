79e35a1e1708e95dbd8d4697590c2c2cca635420
Santiago Benavides <thiago0912b@gmail.com>
2024-04-24 11:20:59 -0500

    esta es la primera version

---
+++ script.js
@@ -0,0 +1,2 @@
+const API_KEY = 'https://v6.exchangerate-api.com/v6/a939caab25cfb5be36fd12c8/latest/USD'; // Reemplaza con tu clave de API de ExchangeRate
+const API_URL = 'https://v6.exchangerate-api.com/v'
